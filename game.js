bb52c3e862c5c75d55ac7491b13444c3d4fe42e0
d3f4u1t3d <pravin09032002@gmail.com>
2022-08-24 12:43:48 +0530

    Game without snake and food logic

---
+++ game.js
@@ -0,0 +1,71 @@
+let lastRenderTime = 0
+let gameOver = false
+const gameBoard = document.getElementById('game-board')
+export let boardContext = gameBoard.getContext('2d')
+export let colorcount = 1;
+let collision_gamestate = 0;
+var foodcolorupdater = false
+var previouscolor = 0
+const radioBtn = document.querySelectorAll('.check');
+radioBtn.forEach((ele) => {
+    // console.log(ele)
+    ele.addEventListener('click', () => rabu(ele.value));
+})
+export function rabu(radiovalue) {
+    // console.log(radiovalue);
+    collision_gamestate = radiovalue === 'True' ? 1 : 0;
+    if (collision_gamestate === 1) gameBoard.classList.add('border');
+    else gameBoard.classList.remove('border');
+    console.log(collision_gamestate);
+}
+
+function main(currentTime) {
+
+
+    if (gameOver) {
+        if (confirm('You Lost. Press ok to restart.')) {
+            window.location = ''
+        }
+        return
+    }
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+    //console.log('Render')
+    lastRenderTime = currentTime
+    //console.log(colorcount)
+    update()
+    draw(colorcount)
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+    updateSnake()
+    foodcolorupdater = updateFood(colorcount)
+    checkDeath()
+
+    if (foodcolorupdater) {
+        if (colorcount == 4) {
+            previouscolor = 4
+            colorcount = 0
+        }
+        else {
+            previouscolor = colorcount
+            ++colorcount
+        }
+        colors.unshift(previouscolor)
+    }
+}
+
+function draw(colorcount) {
+    gameBoard.innerHTML = ''
+    drawFood(gameBoard, colorcount)
+    drawSnake(gameBoard, previouscolor)
+
+}
+function checkDeath() {
+    if (collision_gamestate == 1) { gameOver = outsideGrid(getSnakeHead()) || snakeIntersection() }
+    else { wrap(getSnakeHead()) }
+}
